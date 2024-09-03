59dc4e4f7047ec93bba9dc0ecb12d4b3a41586f6
rapha-vil <rafaela.vilelati@gmail.com>
2024-09-03 20:40:37 -0300

    convertando para numero positivo

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,16 @@
+public class Operadores {
+
+    public static void main(String[] args) {
+
+        int numero = 5;
+
+        numero = -numero;
+
+        System.out.println(numero);
+
+        numero *= -1;
+
+        System.out.println(numero);
+
+    }
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -2,7 +2,7 @@ import java.util.Date;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class TiposVariaveis {
     public static void main(String[] args) {
 
        long id = 3200;
