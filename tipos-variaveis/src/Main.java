7bb2d0f80e063cbef668c4ae415c02ea6dae8938
rapha-vil <rafaela.vilelati@gmail.com>
2024-09-02 21:10:31 -0300

    inclusao dataNascimento

--- tipos-variaveis/src/Main.java
+++ tipos-variaveis/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Date;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -8,10 +10,13 @@ public class Main {
        String nome = "Rafaela Vilela";
        char sexo = 'F';
 
+
+       Date  dataNascimento = new Date();
         System.out.println(id);
         System.out.println(idade);
         System.out.println(nome);
         System.out.println(sexo);
+        System.out.println(dataNascimento);
 
 
     }
