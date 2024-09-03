b82b0b8273ad75ecff007e683ea6c9a1e1dcb46a
rapha-vil <rafaela.vilelati@gmail.com>
2024-09-02 21:01:17 -0300

    exemplos de tipos de variveis

--- tipos-variaveis/src/Main.java
+++ tipos-variaveis/src/Main.java
@@ -2,14 +2,17 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+       long id = 3200;
+       int idade = 37;
+       String nome = "Rafaela Vilela";
+       char sexo = 'F';
+
+        System.out.println(id);
+        System.out.println(idade);
+        System.out.println(nome);
+        System.out.println(sexo);
+
+
     }
 }
